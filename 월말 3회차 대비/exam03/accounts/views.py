57c3023e870c5cec813790938f48d214707f2814
tmddud <tmddud0103@gmail.com>
2021-09-26 21:48:26 +0900

    pre - exam3

---
+++ 월말 3회차 대비/exam03/accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+# Create your views here.
+def index(request):
+    return render(request, 'accounts/index.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == "POST":
+        form =AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form =AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/login.html', context)
+    
